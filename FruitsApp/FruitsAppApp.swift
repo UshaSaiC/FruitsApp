3ad46886326c514be7395ca0a4f3f460193c45a8
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-09-17 19:51:19 +0530

    Initial Commit

---
+++ FruitsApp/FruitsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsAppApp.swift
+//  FruitsApp
+//
+//  Created by Usha Sai Chintha on 17/09/22.
+//
+
+import SwiftUI
+
+@main
+struct FruitsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
